f18315c2d1cfea0682a7f7770669450f87363b46
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-12 20:37:23 +0530

    Code Commit

--- Sol_Demo/Sol_Demo/wwwroot/js/demo.js
+++ Sol_Demo/Sol_Demo/wwwroot/js/demo.js
@@ -4,3 +4,17 @@
             document.getElementById("spanRandom").innerHTML = resolve;
         });
 }
+
+function updateMessageJs() {
+    DotNet.invokeMethodAsync('Sol_Demo', 'OnGetValue','Hello Javascript');
+    console.log("Update Message js Method Call");
+}
+
+function getArrayOfDataJs() {
+   
+    DotNet.invokeMethodAsync('Sol_Demo', 'ReturnArrayAsync')
+            .then(data => {
+                data.push(4);
+                console.log(data);
+            });
+}
